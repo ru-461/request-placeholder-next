7cb0e04e9c6122845933f4327a2b5940ba466735
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-08-06 16:19:22 +0900

    style: Add props to text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,27 @@
 import Link from 'next/link';
-import { Box } from '.';
+import { Box, Text } from '.';
 
 export default function Header() {
   return (
-    <Box>
-      <Link href="/posts">Posts</Link>
-      <Link href="/comments">Comments</Link>
-      <Link href="/albums">Albums</Link>
-      <Link href="/photos">Photos</Link>
-      <Link href="/todos">Todos</Link>
-      <Link href="/users">Users</Link>
+    <Box p="3" display="flex" fontSize="lg">
+      <Text px="1">
+        <Link href="/posts">Posts</Link>
+      </Text>
+      <Text px="1">
+        <Link href="/comments">Comments</Link>
+      </Text>
+      <Text px="1">
+        <Link href="/albums">Albums</Link>
+      </Text>
+      <Text px="1">
+        <Link href="/photos">Photos</Link>
+      </Text>
+      <Text px="1">
+        <Link href="/todos">Todos</Link>
+      </Text>
+      <Text px="1">
+        <Link href="/users">Users</Link>
+      </Text>
     </Box>
   );
 }
