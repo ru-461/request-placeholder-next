1ca13a4c2d11c975f0a64a21fc0cd363ee6feee2
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-08-09 22:10:15 +0900

    style: Change Spinner color

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Box, Spinner } from '@/components';
 export default function Loading() {
   return (
     <Box alignItems="center" display="flex" h="100vh" justifyContent="center">
-      <Spinner size="xl" />
+      <Spinner color="#10B981" size="xl" />
     </Box>
   );
 }
