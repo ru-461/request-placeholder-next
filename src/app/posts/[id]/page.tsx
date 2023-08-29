14d96c949528d10ad9e94662db196dc4f8c88ca5
ru-461 <48552543+ru-461@users.noreply.github.com>
2023-08-29 22:20:14 +0900

    chore: Add eslint:recommended

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -15,7 +15,7 @@ async function fetchPost(id: number) {
 }
 
 async function fetchUser(id: number) {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users/${id}');
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
 
   if (!res.ok) {
     throw new Error('Fetch failed.');
